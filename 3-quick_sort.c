16eb3ae95817bbee5068d5202e184a0d81c7dadd
FelixOtienoArogo <felixarogo@gmail.com>
2022-11-12 04:16:09 +0300

    now

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,86 +1,88 @@
 #include "sort.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <stddef.h>
 
 /**
- * quick_sort - Super fast and impractical sorting function
- * @array: The array to be sorted
- * @size: size of the array
- **/
+ * myswap - swaps two values
+ *
+ * @array: data sort input
+ * @firts: first
+ * @second: second
+ * @size: size
+ *
+ * Return: No Return
+ */
+void myswap(int *array, int firts, int second, int size)
+{
+	int tmp;
+
+	if (array[firts] != array[second])
+	{
+		tmp = array[firts];
+		array[firts] = array[second];
+		array[second] = tmp;
+		print_array(array, size);
+	}
+}
+
+/**
+ * part - part to a pivot
+ *
+ * @array: data sort input
+ * @left: left
+ * @right: right
+ * @size: size input
+ *
+ * Return: New pivote
+ */
+int part(int *array, int left, int right, size_t size)
+{
+	int i = left, j, pivot  = array[right];
+
+	for (j = left; j <= right; j++)
+	{
+		if (array[j] < pivot)
+		{
+			myswap(array, i, j, size);
+			i++;
+		}
+	}
+	myswap(array, i, right, size);
+
+	return (i);
+}
+
+/**
+ * myquicksort -  quick sort algorithm
+ *
+ * @array: data to sort
+ * @left: left pivote
+ * @right: right pivote
+ * @size: size input
+ * Return: No Return
+ */
+void myquicksort(int *array, int left, int right, size_t size)
+{
+	int pivote;
+
+	if (left < right)
+	{
+		pivote = part(array, left, right, size);
+		myquicksort(array, left, pivote - 1, size);
+		myquicksort(array, pivote + 1, right, size);
+	}
+}
+
+/**
+ * quick_sort -  quick sort algorithm
+ *
+ * @array: sort data
+ * @size: size input
+ *
+ * Return: No Return
+ */
 void quick_sort(int *array, size_t size)
 {
-	int *whole_array;
-	size_t full_size;
-
-	if (array == NULL)
+	if (size < 2)
 		return;
-	whole_array = &array[0];
-	full_size = size;
-	quick_sort_really(array, size, whole_array, full_size);
-}
 
-/**
- * quick_sort_really - Used to call the partitioner to get subarrays
- * @array: The array (sub array of last call) to be sorted
- * @size: Size of the array variable
- * @whole_array: The whole array that is to be sorted
- * @full_size: The full size of the array that's being sorted originally
- **/
-void quick_sort_really(int *array, size_t size, int *whole_array,
-		       size_t full_size)
-{
-	size_t part;
-
-	part = 0;
-
-	if (size > 1)
-	{
-		part = partitioner(array, size, whole_array, full_size);
-		quick_sort_really(&array[0], part, whole_array, full_size);
-		quick_sort_really(&array[part], size - part, whole_array, full_size);
-	}
-
-}
-
-/**
- * partitioner - Finds where to split the array and swaps larger items right
- * and smaller items left
- * @array: Size of the array (sub array of last call) being sorted
- * @size: Size of the array variable
- * @whole_array: The whole array that was being sorted
- * @full_size: Full size of the original array
- * Return: The index where the array should be split into two subarrays
- **/
-size_t partitioner(int *array, size_t size, int *whole_array, size_t full_size)
-{
-	int pivot;
-	long front;
-	long end;
-	int temp;
-
-	pivot = array[size - 1];
-	front = -1;
-	end = size;
-
-
-	while (1)
-	{
-		do {
-			front++;
-		} while (array[front] < pivot);
-		do {
-			end--;
-		} while (array[end] > pivot);
-
-		if (front >= end)
-		{
-			return ((size_t) front);
-		}
-		temp = array[front];
-		array[front] = array[end];
-		array[end] = temp;
-		print_array(whole_array, full_size);
-	}
-	return (end);
+	myquicksort(array, 0, size - 1, size);
 }
--- sort.h
+++ sort.h
@@ -22,7 +22,8 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-void quick_sort_really(int *array, size_t size, int *whole_array,size_t full_size);
-size_t partitioner(int *array, size_t size, int *whole_array, size_t full_size);
+void myquicksort(int *array, int left, int right, size_t size);
+int part(int *array, int left, int right, size_t size);
+void myswap(int *array, int firts, int second, int size);
 
 #endif
